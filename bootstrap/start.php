36086eb30f420bc987a08a905b659681b603cbaf
ET <et1103@hotmail.com>
2015-07-14 01:48:09 -0700

    test commit

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'home' => array('DingdingPan-PC'),
+	'home' => array('DingdingPan-PC','ET-Antec'),
 	'local' => array('Peter'),
 
 ));
